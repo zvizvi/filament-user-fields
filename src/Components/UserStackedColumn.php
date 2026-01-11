7557ef6c5b9053b01ddf56d0b39ed8d6e78f5c25
zvizvi <5633910@gmail.com>
2026-01-11 03:57:12 +0200

    Remove Filament v3 support, Bump minimum PHP version to 8.2

--- src/Components/UserStackedColumn.php
+++ src/Components/UserStackedColumn.php
@@ -3,7 +3,6 @@
 namespace Zvizvi\UserFields\Components;
 
 use Filament\Tables\Columns\ImageColumn;
-use Illuminate\Support\Collection;
 
 class UserStackedColumn extends ImageColumn
 {
@@ -16,16 +15,10 @@ class UserStackedColumn extends ImageColumn
             ->stacked()
             ->checkFileExistence(false);
 
-        if (method_exists($this, 'imageHeight')) {
-            $this
-                ->imageHeight(24)
-                ->ring(1)
-                ->tooltip(fn ($state) => $state?->name);
-        } else { // Filament 3
-            $this
-                ->extraImgAttributes(fn () => ['style' => 'width: 24px; height: 24px'])
-                ->tooltip(fn ($state) => ($state instanceof Collection ? $state : collect([$state]))->map(fn ($user) => $user->name)->implode(', '));
-        }
+        $this
+            ->imageHeight(24)
+            ->ring(1)
+            ->tooltip(fn($state) => $state?->name);
     }
 
     public function getImageUrl($userData = null): ?string
