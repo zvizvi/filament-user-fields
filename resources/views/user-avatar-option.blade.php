2990216b01a49b0f9defb5d3180ff8f620f49c30
zvizvi <5633910@gmail.com>
2026-01-28 02:38:55 +0200

    Use filament helper to get user name in avatar component #6

--- resources/views/user-avatar-option.blade.php
+++ resources/views/user-avatar-option.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-2">
     @if($user)
         <x-filament-panels::avatar.user :user="$user" size="sm" />
-        <span class="text-sm font-medium">{{ $user->name }}</span>
+        <span class="text-sm font-medium">{{ filament()->getUserName($user) }}</span>
     @endif
 </div>
